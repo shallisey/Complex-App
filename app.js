3477bbba4b2649a43ed9acafc82b73c309e42437
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-10-17 08:49:53 -0500

    add router file, homepage, and css

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the app');
-});
+const router = require('./router');
+console.log(router);
+
+app.use(express.static('public'));
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.use('/', router);
 
 app.listen(3000);
