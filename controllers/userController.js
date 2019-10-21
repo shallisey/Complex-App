141b670dced35275faed26fbf19129a860c27dea
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-10-21 08:49:02 -0500

    Create Model for User and Validation for registering

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
+const User = require('../models/User');
+
 exports.login = () => {};
 
 exports.logout = () => {};
 
-exports.register = (req, res) => {
-  res.send('Register');
+exports.register = function(req, res) {
+  let user = new User(req.body);
+  user.register();
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send('No errors');
+  }
 };
 
 exports.home = (req, res) => {
