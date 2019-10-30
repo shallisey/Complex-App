9779284980a3667a0dcfffbe01cf848d4bdc4c5b
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-10-30 07:52:09 -0600

    Create views/includes folder and files and Create post form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/User');
 
+exports.mustBeLoggedin = function(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash('errors', 'You must be logged in to perform that action');
+    req.session.save(function() {
+      res.redirect('/');
+    });
+  }
+};
 exports.login = function(req, res) {
   let user = new User(req.body);
   user
@@ -49,10 +59,7 @@ exports.register = function(req, res) {
 
 exports.home = (req, res) => {
   if (req.session.user) {
-    res.render('home-dashboard', {
-      username: req.session.user.username,
-      avatar: req.session.user.avatar
-    });
+    res.render('home-dashboard');
   } else {
     res.render('home-guest', {
       errors: req.flash('errors'),
