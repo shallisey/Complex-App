b5cbbdd4f63bcd2c90932c721f06e89f8f436a46
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-11-12 08:14:35 -0600

    Use DOMPurify to sanitize HTML on front end

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import DOMPurify from 'dompurify';
 
 export default class Search {
   // 1. Select DOM elements, and keep track of any useful data
@@ -64,7 +65,7 @@ export default class Search {
 
   renderResultsHTML(posts) {
     if (posts.length) {
-      this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+      this.resultsArea.innerHTML = DOMPurify.sanitize(`<div class="list-group shadow-sm">
       <div class="list-group-item active"><strong>Search Results</strong> (${
         posts.length > 1 ? `${posts.length} items found` : '1 item found'
       })</div>
@@ -85,7 +86,7 @@ export default class Search {
       </a>`;
         })
         .join('')}
-    </div>`;
+    </div>`);
     } else {
       this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>`;
     }
