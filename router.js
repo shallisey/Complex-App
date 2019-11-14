096250acf0dbbb5cfc4507d785f5459a7b2aa489
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-11-14 08:02:17 -0600

    Let users follow each other and save the 'follow' in the database

--- router.js
+++ router.js
@@ -1,55 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('./controllers/userController');
-const postController = require('./controllers/postController');
+const userController = require("./controllers/userController");
+const postController = require("./controllers/postController");
+const followController = require("./controllers/followController");
 
 //user related routes
 
-router.get('/', userController.home);
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
+router.get("/", userController.home);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
 
 //Profile related
 router.get(
-  '/profile/:username',
+  "/profile/:username",
   userController.ifUserExists,
   userController.profilePostsScreen
 );
 
 // Post related routes
 router.get(
-  '/create-post',
+  "/create-post",
   userController.mustBeLoggedin,
   postController.viewCreateScreen
 );
 
 router.post(
-  '/create-post',
+  "/create-post",
   userController.mustBeLoggedin,
   postController.create
 );
 
-router.get('/post/:id', postController.viewSingle);
+router.get("/post/:id", postController.viewSingle);
 module.exports = router;
 
 router.get(
-  '/post/:id/edit',
+  "/post/:id/edit",
   userController.mustBeLoggedin,
   postController.viewEditScreen
 );
 
 router.post(
-  '/post/:id/edit',
+  "/post/:id/edit",
   userController.mustBeLoggedin,
   postController.edit
 );
 
 router.post(
-  '/post/:id/delete',
+  "/post/:id/delete",
   userController.mustBeLoggedin,
   postController.delete
 );
-router.post('/search', postController.search);
+router.post("/search", postController.search);
+
+// follow related routes
+router.post(
+  "/addFollow/:username",
+  userController.mustBeLoggedin,
+  followController.addFollow
+);
 
 module.exports = router;
