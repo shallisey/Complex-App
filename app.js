046e467920453679e11886f507d1821abf261649
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-12-02 08:26:14 -0600

    Differentiate chat users when chatting // sanitize data.message so users are not open to malicous attacks

--- app.js
+++ app.js
@@ -79,9 +79,15 @@ io.use(function(socket, next) {
 io.on('connection', function(socket) {
   if (socket.request.session.user) {
     let user = socket.request.session.user;
+
+    socket.emit('welcome', { username: user.username, avatar: user.avatar });
+
     socket.on('chatMessageFromBrowser', function(data) {
-      io.emit('chatMessageFromServer', {
-        message: data.message,
+      socket.broadcast.emit('chatMessageFromServer', {
+        message: sanitizeHTML(data.message, {
+          allowedTags: [],
+          allowedAttributes: []
+        }),
         username: user.username,
         avatar: user.avatar
       });
--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,3 +1,5 @@
+import DOMPurify from 'dompurify';
+
 export default class Chat {
   constructor() {
     this.openedYet = false;
@@ -27,6 +29,18 @@ export default class Chat {
     this.socket.emit('chatMessageFromBrowser', {
       message: this.chatField.value
     });
+    this.chatLog.insertAdjacentHTML(
+      'beforeend',
+      DOMPurify.sanitize(`<div class="chat-self">
+      <div class="chat-message">
+        <div class="chat-message-inner">
+          ${this.chatField.value}
+        </div>
+      </div>
+      <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+    </div>`)
+    );
+    this.chatLog.scrollTop = this.chatLog.height;
     this.chatField.value = '';
     this.chatField.focus();
   }
@@ -41,10 +55,15 @@ export default class Chat {
     }
     this.openedYet = true;
     this.chatWrapper.classList.add('chat--visible');
+    this.chatField.focus();
   }
 
   openConnection() {
     this.socket = io();
+    this.socket.on('welcome', data => {
+      this.username = data.username;
+      this.avatar = data.avatar;
+    });
     this.socket.on('chatMessageFromServer', data => {
       this.displayMessageFromServer(data);
     });
@@ -53,15 +72,16 @@ export default class Chat {
   displayMessageFromServer(data) {
     this.chatLog.insertAdjacentHTML(
       'beforeend',
-      `
-    <div class="chat-other">
-        <a href="#"><img class="avatar-tiny" src="${data.avatar}"></a>
-        <div class="chat-message"><div class="chat-message-inner">
-          <a href="#"><strong>${data.username}:</strong></a>
-          ${data.message}
-        </div></div>
-      </div>`
+      DOMPurify.sanitize(`
+      <div class="chat-other">
+          <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+          <div class="chat-message"><div class="chat-message-inner">
+            <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
+            ${data.message}
+          </div></div>
+        </div>`)
     );
+    this.chatLog.scrollTop = this.chatLog.height;
   }
 
   injectHTML() {
