3b28ef2eb65ee6ad395e13176a3d7e1684417d8b
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-12-11 08:38:57 -0600

    Add form validation for password by checking if the password is between 12 and 50 characters // make sure all fields are validated on submit

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default class RegistrationForm {
   constructor() {
+    this.form = document.querySelector('#registration-form');
     this.allFields = document.querySelectorAll(
       '#registration-form .form-control'
     );
@@ -10,20 +11,59 @@ export default class RegistrationForm {
     this.username.previousValue = '';
     this.email = document.querySelector('#email-register');
     this.email.previousValue = '';
+    this.password = document.querySelector('#password-register');
+    this.password.previousValue = '';
+    this.username.isUnique = false;
+    this.email.isUnique = false;
     this.events();
   }
 
   // Events
   events() {
+    this.form.addEventListener('submit', e => {
+      e.preventDefault();
+      this.formSubmitHandler();
+    });
+
     this.username.addEventListener('keyup', () => {
       this.isDifferent(this.username, this.usernameHandler);
     });
     this.email.addEventListener('keyup', () => {
       this.isDifferent(this.email, this.emailHandler);
     });
+    this.password.addEventListener('keyup', () => {
+      this.isDifferent(this.password, this.passwordHandler);
+    });
+    this.username.addEventListener('blur', () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+    this.email.addEventListener('blur', () => {
+      this.isDifferent(this.email, this.emailHandler);
+    });
+    this.password.addEventListener('blur', () => {
+      this.isDifferent(this.password, this.passwordHandler);
+    });
   }
 
   // Methods
+  formSubmitHandler() {
+    this.usernameImmediately();
+    this.usernameAfterDelay();
+    this.emailAfterDelay();
+    this.passwordImmediately();
+    this.passwordAfterDelay();
+
+    if (
+      this.username.isUnique &&
+      !this.username.errors &&
+      this.email.isUnique &&
+      !this.email.errors &&
+      !this.password.errors
+    ) {
+      this.form.submit();
+    }
+  }
+
   isDifferent(el, handler) {
     if (el.previousValue != el.value) {
       handler.call(this);
@@ -39,6 +79,34 @@ export default class RegistrationForm {
       () => this.usernameAfterDelay(),
       800
     );
+  }
+
+  passwordHandler() {
+    this.password.errors = false;
+    this.passwordImmediately();
+    clearTimeout(this.password.timer);
+    this.password.timer = setTimeout(() => this.passwordAfterDelay(), 800);
+  }
+
+  passwordImmediately() {
+    if (this.password.value.length > 50) {
+      this.showValidationError(
+        this.password,
+        'Password cannot exceed 50 characters'
+      );
+    }
+    if (!this.password.errors) {
+      this.hideValidationError(this.password);
+    }
+  }
+
+  passwordAfterDelay() {
+    if (this.password.value.length < 12) {
+      this.showValidationError(
+        this.password,
+        'Password must exceed 12 characters'
+      );
+    }
   }
 
   emailHandler() {
@@ -90,7 +158,7 @@ export default class RegistrationForm {
     if (this.username.value.length > 30) {
       this.showValidationError(
         this.username,
-        'Username can not exceed 30 characters.'
+        'Username cannot exceed 30 characters.'
       );
     }
 
