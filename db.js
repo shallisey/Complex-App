db8257861de34bbb57b1727691987c80c8f3af9a
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-12-17 08:20:32 -0600

    change app.listen

--- db.js
+++ db.js
@@ -8,6 +8,12 @@ mongodb.connect(
   (err, client) => {
     module.exports = client;
     const app = require('./app');
-    app.listen(process.env.PORT);
+    app.listen(process.env.PORT || 3000, function() {
+      console.log(
+        'Express server listening on port %d in %s mode',
+        this.address().port,
+        app.settings.env
+      );
+    });
   }
 );
